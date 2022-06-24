1262ad7db4ac6c31306634f7dbee83ce3a7ebc8d
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-24 11:32:53 +0200

    Aggiornamento graffe

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,5 @@
 WORK IN PROGRESS
 
-<a href=" { route('login') } "> Login </a>
+<a href=" {{ route('login') }} "> Login </a>
 
-<a href=" { route('register') } "> Registrati </a>
+<a href=" {{ route('register') }} "> Registrati </a>
