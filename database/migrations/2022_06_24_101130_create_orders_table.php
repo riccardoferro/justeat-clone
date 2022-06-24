f877401db31b6381c3c9d76cfa82933caf2f1521
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-24 12:30:31 +0200

    Creata Migration per la tabella ordini ' Orders '

---
+++ database/migrations/2022_06_24_101130_create_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->required();
+            $table->string('surname')->required();
+            $table->string('email')->required()->unique();
+            $table->string('address')->required();
+            $table->string('phone')->required();
+            $table->boolean('status')->required();
+            $table->string('payment')->required();
+            $table->float('total')->required();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
