804d38e735cb416e58caa716b2235b1185637d26
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-06-24 17:14:50 +0200

    aggiunta elementi mancanti di default

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+
 class RegisterController extends Controller
 {
     /*
@@ -68,6 +69,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'company' => $data['company'],
+            'address' => $data['address'],
+            'partita_iva' => $data['partita_iva']
         ]);
     }
 }
