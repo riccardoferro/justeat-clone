43b7dc6362003ec4ce0a3e9c6170ca2089891669
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-24 17:48:35 +0200

    creato db categorie per il seeder

---
+++ config/categories.php
@@ -0,0 +1,27 @@
+
+
+<?php
+
+return [
+    [
+        "name" => "italiana",
+        "image" => "immagina_cucina_italiana",
+    ],
+    [
+      "name" => "messicana",
+      "image" => "immagina_cucina_messicana",
+    ],
+    [
+      "name" => "cinese",
+      "image" => "immagina_cucina_cinese",
+    ],
+    [
+      "name" => "giapponese",
+      "image" => "immagina_cucina_giapponese",
+    ],
+    [
+      "name" => "americana",
+      "image" => "immagina_cucina_americana",
+    ],
+
+];
