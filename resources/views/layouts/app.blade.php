f4d87b8d1f7d21edaf92d58951935bfda5d87cb6
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-27 18:24:54 +0200

    creazione pagine crud + inizio create.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
