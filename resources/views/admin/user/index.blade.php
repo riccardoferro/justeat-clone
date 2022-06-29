2638267d96f270e56b2a1b4b5e0409b91e004bb2
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-29 22:51:58 +0200

    inizio inde user

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,28 +1,41 @@
-<p>Benvenuto {{ $user->name }}</p>
-<h1>I tuoi Dati</h1>
-<p>{{ $user->email }}</p>
-<p>{{ $user->company }}</p>
-<p>{{ $user->address }}</p>
-<p>{{ $user->partita_iva }}</p>
+@extends('layouts.app')
 
-<img src="{{ asset('storage/' . $user->image) }}" alt="image-company" style="width:150px">
-<div>
-    <a href="{{ route('admin.user.edit', $user->id) }}">Inserisci una nuova Immagine Profilo</a>
-</div>
+@section('content')
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-12">
+                <h2>Benvenuto {{ $user->name }}!</h2>
+                <h4>In questa sezione potrai visualizzare e modificare i tuoi dati</h4>
+            </div>
+        </div>
+        <h1>I tuoi Dati</h1>
+        <p>{{ $user->email }}</p>
+        <p>{{ $user->company }}</p>
+        <p>{{ $user->address }}</p>
+        <p>{{ $user->partita_iva }}</p>
+
+        <img src="{{ asset('storage/' . $user->image) }}" alt="image-company" style="width:150px">
+        <div>
+            <a href="{{ route('admin.user.edit', $user->id) }}">Inserisci una nuova Immagine Profilo</a>
+        </div>
 
 
-<h1>Le tue Categorie</h1>
+        <h1>Le tue Categorie</h1>
 
-@if (count($user->categories))
-    @foreach ($user->categories as $category)
-        <p>{{ $category->name }}</p>
-        <img src="{{ asset('storage/images/category_img/' . $category->image) }}" alt="{{ $category->name }}"
-            style="width:150px">
-    @endforeach
-    <a href="{{ route('admin.categories.edit', $user->id) }}">Modifica le tue categorie</a>
-@else
-    <p>Al momento non possiedi alcuna categoria</p>
-    <a href="{{ route('admin.categories.create', $user->id) }}">Aggiungi categoria</a>
-@endif
+        @if (count($user->categories))
+            @foreach ($user->categories as $category)
+                <p>{{ $category->name }}</p>
+                <img src="{{ asset('storage/images/category_img/' . $category->image) }}" alt="{{ $category->name }}"
+                    style="width:150px">
+            @endforeach
+            <a href="{{ route('admin.categories.edit', $user->id) }}">Modifica le tue categorie</a>
+        @else
+            <p>Al momento non possiedi alcuna categoria</p>
+            <a href="{{ route('admin.categories.create', $user->id) }}">Aggiungi categoria</a>
+        @endif
+    </div>
 
-{{-- Da inserire una crud per la modifica delle categorie dei Ristoranti --}}
+
+    {{-- Da inserire una crud per la modifica delle categorie dei Ristoranti --}}
+
+@endsection
