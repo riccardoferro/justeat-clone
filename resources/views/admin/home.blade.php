98ffbd67bc08c1a78fddc5f6b9f3a3192b21e253
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-29 19:15:05 +0200

    modifica layout-home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,31 +2,28 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card t4-bg-dash">
-                    <div class="card-body">
+        <div class="row justify-content-center align-items-center t4-h80vh">
+            <div class="col-xxl-12 col-xl-12 col-md-12 col-1 col-sm-1 t4-card">
+                <div class="row flex-column align-items-center justify-content-center t4-container-text">
+                    <div class="col-12">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
 
-                        {{ __('Hai effettuato l\'accesso!') }}
+                        <h2 class="t4-orange-title">{{ __('Hai effettuato l\'accesso!') }}</h2>
                     </div>
-                    <div class="row justify-content-center">
-                        <div class="col-4">
-                            <button href="{{ route('admin.user.index') }}" class="btn t4-add-btn">
-                                Vai alla Dashboard
-                                <span class="t4-dashboard-img ms-2"><img src="/images/report.png"
-                                        alt="dashboard-logo"></span>
-                            </button>
-                        </div>
+                    <div class="col-8">
+                        <a href="{{ route('admin.user.index') }}" class="btn t4-add-btn t4-bg-black ">
+                            Vai alla Dashboard
+                            <span class="t4-dashboard-img ms-2"><img src="/images/report.png" alt="dashboard-logo"></span>
+                        </a>
                     </div>
-
                 </div>
 
             </div>
+
         </div>
     </div>
 @endsection
