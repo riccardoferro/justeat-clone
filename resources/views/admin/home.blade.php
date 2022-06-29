1d800af8a3588c0ce6b2aca04803f80199b93614
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-29 17:51:50 +0200

    modifica button home blade

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,9 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card t4-bg-dash">
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
@@ -16,9 +14,16 @@
 
                         {{ __('Hai effettuato l\'accesso!') }}
                     </div>
-                    <a href="{{ route('admin.user.index') }}" class="btn btn-primary">
-                        User
-                    </a>
+                    <div class="row justify-content-center">
+                        <div class="col-4">
+                            <button href="{{ route('admin.user.index') }}" class="btn t4-add-btn">
+                                Vai alla Dashboard
+                                <span class="t4-dashboard-img ms-2"><img src="/images/report.png"
+                                        alt="dashboard-logo"></span>
+                            </button>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
