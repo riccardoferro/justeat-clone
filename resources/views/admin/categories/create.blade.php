66ab4668a5bc6ae79e33075a3699b878adba451d
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-30 22:49:33 +0200

    modifica button indietro

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -16,51 +16,56 @@
                     </span>
                 </h2>
                 <h4>Seleziona una o più categorie tra Pizza, Fast Food e tante altre!</h4>
-                <div class="col-xxl-1 col-xl-2 col-lg-2 col-md-2 col-sm-2 col-6">
-                    <a href="{{ route('admin.user.index') }}" class="btn t4-add-btn d-flex align-items-center">
-                        <span class="me-2">Indietro </span>
-                        <img src="/images/replay.png" alt="">
-                    </a>
+            </div>
+        </div>
+
+    </div>
+
+
+    <form action="{{ route('admin.categories.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+
+        <div class="form-group d-flex align-items-center">
+            <div class="row justify-content-center mt-5 ">
+                @foreach ($categories as $category)
+                    {{-- name is an array --}}
+                    <div class="col-auto d-flex flex-column t4-card-categories">
+                        <div class="d-flex ms-2">
+                            <input class="form-check-input" type="checkbox" value="{{ $category->id }}"
+                                name="categories[]"
+                                {{ in_array($category->id, old('categories', [])) ? 'checked' : '' }} />
+                            <div class="form-check-label ms-2"> {{ $category->name }} </div>
+                        </div>
+
+                        <img class="t4-img-categories"
+                            src="{{ asset('storage/images/category_img/' . $category->image) }}"
+                            alt="{{ $category->name }}" style="width:150px">
+                    </div>
+                @endforeach
+
+                @error('categories[]')
+                    <div class="invalid-feedback"> {{ $message }} </div>
+                @enderror
+                <div class="form-group d-flex justify-content-center mt-5">
+                    <button type="submit" class="btn t4-add-btn">
+                        Aggiungi le tue categorie
+                        <span>
+                            <img src="/images/plus.png" alt="">
+                        </span>
+                    </button>
+
+                    <div class="ms-4">
+                        <a href="{{ route('admin.user.index') }}" class="btn t4-add-btn text-center">
+                            <div class=" d-flex align-items-center">
+                                <span class="me-1">Indietro</span>
+                                <img src="/images/replay.png" alt="">
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
 
-
-        <form action="{{ route('admin.categories.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-
-            <div class="form-group d-flex align-items-center">
-                <div class="row justify-content-center mt-5 ">
-                    @foreach ($categories as $category)
-                        {{-- name is an array --}}
-                        <div class="col-auto d-flex flex-column t4-card-categories">
-                            <div class="d-flex ms-2">
-                                <input class="form-check-input" type="checkbox" value="{{ $category->id }}"
-                                    name="categories[]"
-                                    {{ in_array($category->id, old('categories', [])) ? 'checked' : '' }} />
-                                <div class="form-check-label ms-2"> {{ $category->name }} </div>
-                            </div>
-
-                            <img class="t4-img-categories"
-                                src="{{ asset('storage/images/category_img/' . $category->image) }}"
-                                alt="{{ $category->name }}" style="width:150px">
-                        </div>
-                    @endforeach
-
-                    @error('categories[]')
-                        <div class="invalid-feedback"> {{ $message }} </div>
-                    @enderror
-                    <div class="form-group d-flex justify-content-center mt-5">
-                        <button type="submit" class="btn t4-add-btn">
-                            Aggiungi le tue categorie
-                            <span>
-                                <img src="/images/plus.png" alt="">
-                            </span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-        </form>
+    </form>
     </div>
 @endsection
