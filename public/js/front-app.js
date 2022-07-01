9dc1cf4419bbedb2207243ccb4451fc22e443ef3
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-07-01 18:40:38 +0200

    push test

--- public/js/front-app.js
+++ public/js/front-app.js
@@ -58343,7 +58343,7 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\crist\Desktop\Booleat\booleat\resources\js\front-app.js */"./resources/js/front-app.js");
+module.exports = __webpack_require__(/*! /Users/danielegelsomino/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Booleanprojects/booleat/resources/js/front-app.js */"./resources/js/front-app.js");
 
 
 /***/ })
