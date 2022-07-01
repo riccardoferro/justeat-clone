2d29f1f28bd8c5b5e7e16bc83aa905e25d2faa24
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-07-01 02:03:11 +0200

    aggiunta conferma elimanazione piatto

--- public/js/app.js
+++ public/js/app.js
@@ -53721,19 +53721,36 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+// Funzione Incremento e Decremento Prezzo
 document.addEventListener("DOMContentLoaded", function () {
+  var price = document.getElementById('price-input');
   document.getElementById('increment').addEventListener('click', function () {
-    document.getElementById('price-input').value++;
+    price.value++;
   });
-  console.log('ok');
   document.getElementById('decrement').addEventListener('click', function () {
-    if (document.getElementById('price-input').value > 0) {
-      document.getElementById('price-input').value--;
+    // decrementa solamente il prezzo superiore a 0
+    if (price.value > 0) {
+      price.value--;
     } else {
-      document.getElementById('price-input').value = 0;
+      price.value = 0;
     }
+
+    ;
   });
-  console.log('Ok 2');
+}); // Funzione per la conferma cancellazione
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Prende Elemento Div conferma Eliminazione
+  var deleteForm = document.getElementById('t4-delete-form');
+  deleteForm.style.transition = 'all 2s'; // mostra la div controllo
+
+  document.getElementById('t4-delete-span').addEventListener('click', function () {
+    deleteForm.style.display = 'flex';
+  }); // nasconde la div di controllo
+
+  document.getElementById('t4-cancel-delete').addEventListener('click', function () {
+    deleteForm.style.display = 'none';
+  });
 });
 
 /***/ }),
--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -25,30 +25,14 @@
         </div>
 
         <div class="row flex-column justify-content-center">
-            {{-- <div class="col-12 text-alig-center d-flex justify-content-center flex-sm-column flex-xl-row">
-                <div class="col-xl-9 col-md-9 col-sm-12">
-                    <h1>Il tuo menù</h1>
-                </div>
-
-
-
-                <div class="ms-a">
-                    <a href="{{ route('admin.plates.create') }}" class="btn t4-add-btn d-flex align-items-center">
-                        <span class="me-2">Aggiungi </span>
-                        <img src="/images/add2.png" alt="">
-                    </a>
-                </div>
-
-            </div> --}}
-
-
-
 
             <div class="row col-xxl-10 col-xl-10 col-md-11 col-sm-12 m-auto mt-3 justify-content-center">
 
                 @if (count($plates))
                     @foreach ($plates as $plate)
                         <div class="col-sm-10 col-md-5 col-xl-3  d-flex flex-column t4-card ">
+
+                            {{-- ////////// Card Piatto --}}
                             <div class="t4-card-img">
                                 <img src="{{ asset('storage/' . $plate->image) }}" alt="{{ $plate->name }}">
                             </div>
@@ -69,19 +53,22 @@
                                     <p style="color:rgb(165, 4, 4)">Non disponibile</p>
                                 @endif
                             </div>
+                            {{-- Fine Card piatto \\\\\\\\\\\\\\ --}}
+
+                            {{-- /////////////   Sezione Bottoni --}}
                             <div class="t4-card-buttons
                                 d-flex justify-content-between">
-                                {{-- MODIFICA PIATTO --}}
 
-
+                                {{-- ///////   MODIFICA PIATTO --}}
                                 <a href="{{ route('admin.plates.edit', ['plate' => $plate->id]) }}"
                                     class="btn t4-add-btn d-flex align-items-center">
                                     <span class="me-2">Modifica </span>
                                     <img src="/images/edit-document.png" alt="">
                                 </a>
-                                {{-- <span class='open-form' id="open-form">Elimina</span> --}}
-                                {{-- ELIMINA PIATTO --}}
+                                {{-- fine modifica Piatto  \\\\\\\\\ --}}
 
+
+                                {{-- ////////// ELIMINA PIATTO --}}
                                 <form action="{{ route('admin.plates.destroy', ['plate' => $plate->id]) }}"
                                     class="d-inline-block" method="POST">
 
@@ -90,33 +77,19 @@
 
 
 
-                                    <button type="submit" class="btn t4-add-btn d-flex align-items-center">
+                                    <button id='delete_button' type="submit"
+                                        class="btn t4-add-btn d-flex align-items-center">
                                         <span class="me-2">Elimina</span>
                                         <img src="/images/delete.png" alt="">
                                     </button>
                                 </form>
+                                {{-- Fine Elimina piatto \\\\\\\\\ --}}
+
                             </div>
+                            {{-- Fine Sezione Bottoni \\\\\\\\\\\ --}}
+
+
                         </div>
-                        {{-- <form id="t4-delete-page" action="{{ route('admin.plates.destroy', ['plate' => $plate->id]) }}"
-                            method="POST">
-
-                            @csrf
-                            @method('DELETE')
-                            <h1>Sei sicuro di voler eliminare?</h1>
-                            <div class="t4-delete-page-inner">
-                                <button type="submit" class="btn t4-add-btn d-flex align-items-center">
-                                    <span class="me-2">Elimina</span>
-                                    <img src="/images/delete.png" alt="">
-                                </button>
-                                <span id='close-form'>Annulla</span>
-
-
-                            </div>
-
-
-
-
-                        </form> --}}
                     @endforeach
                 @else
                     <div class="col-4 text-center mt-5">
@@ -156,22 +129,6 @@
 
 @endsection
 
-{{-- <script defer>
-    window.onload = function() {
 
 
-        document.querySelectorAll(".open-form").forEach(elem => elem.addEventListener("click",
-            () => {
-                document.getElementById("t4-delete-page").style.display = "flex";
-            }));
-        document.querySelectorAll("#close-form").forEach(elem => elem.addEventListener("click",
-            () => {
-                document.getElementById("t4-delete-page").style.display = "none";
-            }));
-        console.log('Console.log 3');
-
-
-        console.log('Console.log 4');
-
-    }
-</script> --}}
+<script></script>
