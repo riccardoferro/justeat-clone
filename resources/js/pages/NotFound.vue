07c0950d8deec3b3fc6f739e0a0f339c7a5b0045
riccardoferro <riccardo.ferron8@gmail.com>
2022-07-03 15:17:48 +0200

    Inserito titolo h3

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3 class="text-center pt-5 mt-5">
+        NOT FOUND PAGE VUE
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
