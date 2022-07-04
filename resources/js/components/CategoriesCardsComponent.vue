bc9f142e7e88d99874ab43a73650d349328a3e63
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-07-04 10:47:41 +0200

    modifica sulle card nella home

--- resources/js/components/CategoriesCardsComponent.vue
+++ resources/js/components/CategoriesCardsComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-      <div class="text-center py-5">
-          <h2>Cerchi qualcos'altro?</h2>
-          <h4>Scegli tra tutte le categorie disponibili</h4>
-      </div>
+    <div class="text-center py-5">
+      <h2>Cerchi qualcos'altro?</h2>
+      <h4>Scegli tra tutte le categorie disponibili</h4>
+    </div>
 
-      <div class="row t4-bg-orange">
-          <!-- <div
+    <div class="row t4-bg-orange">
+      <!-- <div
               class="
                 col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12
                 t4-category-label
@@ -18,45 +18,58 @@
               <img :src="'/images/category_img/' + category.image" alt="" />
           </div> -->
 
-
-            <!-- inizio card -->
+      <!-- inizio card -->
       <div
-        class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 mb-5 d-flex flex-column align-items-center t4-category-label"
+        class="
+          col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12
+          mb-5
+          d-flex
+          flex-column
+          align-items-center
+          t4-category-label
+        "
         v-for="category in categories"
         :key="category.id"
       >
         <!-- SEZIONE CHE FARA' VEDERE TUTTI I RISTORANTI AVENTI QUELLA CATEGORIA SELEZIONATA  -->
         <router-link
-          :to="{ name: 'category-restaurants-filter', params: { slug: category.slug } }"
+          :to="{
+            name: 'category-restaurants-filter',
+            params: { slug: category.slug },
+          }"
         >
-            <!-- FINE SEZIONE SINGOLO RISTORANTE  -->
-            <div>
-              <h2>{{ category.name }}</h2>
-              
-                <!-- <h6 class="t4-orange-text t4-fw-6 d-flex align-items-center">
+          <!-- FINE SEZIONE SINGOLO RISTORANTE  -->
+          <div>
+            <h2 class="t4-orange-text">{{ category.name }}</h2>
+
+            <!-- <h6 class="t4-orange-text t4-fw-6 d-flex align-items-center">
                   {{ user.company }}
                   <span class="t4-icon-company ms-2">
                     <img src="/images/posate.png" alt="" />
                   </span>
                 </h6> -->
-                <!-- <h6 class="mb-3 t4-fw-6">
+            <!-- <h6 class="mb-3 t4-fw-6">
                   <span class="t4-orange-text">Categorie:</span>
                   <span v-for="category in user.categories" :key="category.slug">
                     {{ category.name }}
                   </span>
                 </h6> -->
-            </div>
-            <div class=" col-xxl-8 col-xl-8 col-lg-8 col-md-10 col-sm-12 col-12 t4-img-company">
-                <img :src="'/images/category_img/' + category.image" alt="immagine categoria" />
-            </div>
-
+          </div>
+          <div
+            class="
+              col-xxl-8 col-xl-8 col-lg-8 col-md-10 col-sm-12 col-12
+              t4-img-company
+            "
+          >
+            <img
+              :src="'/images/category_img/' + category.image"
+              alt="immagine categoria"
+            />
+          </div>
         </router-link>
       </div>
       <!-- fine card -->
-
-
-
-      </div>
+    </div>
   </div>
 </template>
 
