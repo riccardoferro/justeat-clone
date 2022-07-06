60308880757c846de0a7fe73e80c4557fbfe3e2c
riccardoferro <riccardo.ferron8@gmail.com>
2022-07-06 02:23:31 +0200

    Creato service per far funzionare BrainTree

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+
+use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //inietteremo un service di modo da far funzionare BrainTree
+        $this->app->singleton(Gateway::class, function($app){
+
+            
+
+
+            // come parametro passeremo la configurazione di BrainTree che abbiamo sul file ENV
+            return new Gateway(
+                [
+                    
+                'environment' => getenv('BT_ENVIRONMENT'),
+                'merchantId' => getenv('BT_MERCHANT_ID'),
+                'publicKey' => getenv('BT_PUBLIC_KEY'),
+                'privateKey' => getenv('BT_PRIVATE_KEY')
+                
+                ]
+            );
+        });
+
     }
 }
